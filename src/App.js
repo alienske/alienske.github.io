0faa3c4fd49bfdca905eecc9dd389dfce1bad904
Petermuturi <pmuturi65@gmail.com>
2017-01-27 19:03:16 +0300

    initial setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Up and running
+      </div>
+    );
+  }
+}
+
+export default App
